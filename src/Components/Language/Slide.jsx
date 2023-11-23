3d58c4945caa296b27c448963ae331b51a940217
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2023-11-23 19:53:03 +0200

    text update

--- src/Components/Language/Slide.jsx
+++ src/Components/Language/Slide.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import netflix from '../../images/product.jpeg'
 import coin from '../../images/coin.png'
+import checkmark from '../../images/checkmark.png'
 var mixpanel = require('mixpanel-browser');
 
 function Slide({ languageData, targetURL, uid }) {
@@ -19,20 +20,28 @@ function Slide({ languageData, targetURL, uid }) {
         <div className='max-w-[1135px] p-5 m-auto mt-[20px] md:mt-[50px] bg-gray/25 rounded-[14px] border-[1px] border-[#088CD9]'>
             <div className='flex items-center gap-3'>
                 <img className=' h-[60px]' src={netflix} alt="Image" />
-                <p className='text-white font-bold text-[18px]'>{languageData?.OfferTitle}</p>
+                <p className='text-white font-bold text-[22px]'>{languageData?.OfferTitle}</p>
             </div>
 
             <div className='gradient-line w-full h-[2px] my-4'></div>
 
-            <p className='text-white text-[14px] font-regular saira min-h-[70px] mb-6 md:mb-4'>{languageData?.OfferDescription}</p>
-            <div className='flex justify-between items-center'>
+            <ul>
+                {
+                    languageData?.listTexts.map((item, index) => (
+                        <li key={index} className='flex items-center gap-1 mb-1'>
+                            <p className='text-white text-[18px] font-regular saira'><span className='mr-1'>✅ </span>  {item}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+            <div className='flex justify-between items-center mt-4'>
                 <div>
                     <div className='flex gap-2'>
                         <img src={coin} className='w-6 h-6' alt="Coin" />
                         <p className='font-bold text-[18px] text-white'>1,000</p>
 
                     </div>
-                    <p className='saira text-[16px]'>{languageData?.TreatCoins}</p>
+                    <p className='saira text-[20px]'>{languageData?.TreatCoins}</p>
                 </div>
 
                 <div>
@@ -42,8 +51,8 @@ function Slide({ languageData, targetURL, uid }) {
                             distinct_id: uid || 'not_set'
                         });
                         window.location.replace(targetURL);
-                    }}> <button className='px-[25px] py-[10px] saira gradient'>{languageData?.OfferButton}</button></a>
-                    <p onClick={e => handleCopy()} className={`underline saira text-center text-[12px] text-white mt-2 duration-300 cursor-pointer ${isLinkCopied === true ? 'text-[#088CD9]' : 'text-white'}`}>{isLinkCopied === true ? languageData?.LinkCopied : languageData?.CopyLink}</p>
+                    }}> <button className='px-[25px] py-[10px] saira gradient text-[20px]'>{languageData?.OfferButton}</button></a>
+                    <p onClick={e => handleCopy()} className={`underline saira text-center text-[16px] text-white mt-2 duration-300 cursor-pointer ${isLinkCopied === true ? 'text-[#088CD9]' : 'text-white'}`}>{isLinkCopied === true ? languageData?.LinkCopied : languageData?.CopyLink}</p>
                 </div>
             </div>
         </div>
