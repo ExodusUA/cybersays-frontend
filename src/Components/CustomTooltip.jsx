50394902f98a6bfba71acff218996723998ff680
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2024-01-11 19:34:17 +0200

    Revert "Merge branch 'staging'"
    This reverts commit 6282b267e97124dcffe811601ce5c17469f6b8ae, reversing
    changes made to 5d4f38b564e674e4260932a8d1f71dadbdab2cc0.

--- src/Components/CustomTooltip.jsx
+++ src/Components/CustomTooltip.jsx
@@ -1,6 +1,6 @@
 function CustomTooltip({ open, setOpen, children }) {
     return (
-        <div className={`absolute min-w-[370px] right-0 bg-white p-4 rounded-[22px] duration-300 z-[999999999999] ${open === true ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
+        <div className={`absolute min-w-[400px] right-[-250px] bg-white p-4 rounded-lg duration-300 z-[999999999999] ${open === true ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
              <p className="underline text-black cursor-pointer" onClick={e => setOpen(false)}>Close</p>
             {children}
         </div>
