0b8f5d6ca95efeb5a411ad5f528c54abdf7df43a
Danichok <danielreabyviktorovych@gmail.com>
2024-02-07 22:36:29 +0200

    Update LeaderboardList.jsx

--- src/Components/LeaderboardList.jsx
+++ src/Components/LeaderboardList.jsx
@@ -50,7 +50,7 @@ function LeaderboardList({ leaderboardData, languageData, day }) {
                     <p className='saira text-[14px] md:text-[14px] font-medium text-[#FFED63] text-center'>{leaderboardData.leaderboard[2].points} {languageData?.leaderboardsPoints} </p>
                 </div>
             </div>
-            <div className='h-[240px] md:h-[600px] overflow-scroll'>
+            <div className='h-[240px] md:h-[350px] overflow-scroll'>
                 {
                     leaderboardData.leaderboard.length < 4
                         ? <div className='w-full flex justify-center my-8'>
@@ -58,20 +58,23 @@ function LeaderboardList({ leaderboardData, languageData, day }) {
                         </div>
 
                         : leaderboardData.leaderboard.slice(3).map((user, index) => (
-                            <div className={`bg-[#EAEAEA] ${design === '0' ? 'bg-opacity-40 ' : 'bg-opacity-20 '} backdrop-blur-lg rounded-[30px] text-center flex pr-3  justify-between items-center mt-2`}>
-                                <div className='flex items-center'>
-                                    <img className=' rounded-full w-[40px] h-[40px]' src={twoSt} alt="twoSt" />
-                                    <p className='saira text-[12px] md:text-[14px] font-medium text-center ml-[15px] truncate w-[120px]'>{user?.email}</p>
+                            <div className=' '>
+                                <div className={`bg-[#EAEAEA] ${design === '0' ? 'bg-opacity-40 ' : 'bg-opacity-20 '} backdrop-blur-lg rounded-[30px] text-center flex pr-3  justify-between items-center mt-2`}>
+                                    <div className='flex items-center'>
+                                        <img className=' rounded-full w-[40px] h-[40px]' src={twoSt} alt="twoSt" />
+                                        <p className='saira text-[12px] md:text-[14px] font-medium text-center ml-[15px] truncate w-[120px]'>{user?.email}</p>
+                                    </div>
+                                    <div className='leading-[14px]'>
+                                        <p className={`saira text-[12px] md:text-[14px] font-medium ${design === '0' ? 'text-[#FFED63]' : 'gradient-timeCounter'}`}>$6.90</p>
+                                        <p className='saira text-[12px] md:text-[14px] font-medium'>{languageData?.leaderboardsPrize}</p>
+                                    </div>
+                                    <div className='leading-[14px]'>
+                                        <p className={`saira text-[12px] md:text-[14px] font-medium text-[#FFED63] ${design === '0' ? 'text-[#FFED63]' : 'gradient-timeCounter'}`}>{user?.points.toFixed(0)}</p>
+                                        <p className='saira text-[12px] md:text-[14px] font-medium'>{languageData?.leaderboardsPoints}</p>
+                                    </div>
+                                    <p className={`saira text-[16px] font-bold ${design === '0' ? 'text-[#FFED63]' : 'gradient-timeCounter'}`}>#{index + 4}</p>
                                 </div>
-                                <div className='leading-[14px]'>
-                                    <p className={`saira text-[12px] md:text-[14px] font-medium ${design === '0' ? 'text-[#FFED63]' : 'gradient-timeCounter'}`}>$6.90</p>
-                                    <p className='saira text-[12px] md:text-[14px] font-medium'>{languageData?.leaderboardsPrize}</p>
-                                </div>
-                                <div className='leading-[14px]'>
-                                    <p className={`saira text-[12px] md:text-[14px] font-medium text-[#FFED63] ${design === '0' ? 'text-[#FFED63]' : 'gradient-timeCounter'}`}>{user?.points.toFixed(0)}</p>
-                                    <p className='saira text-[12px] md:text-[14px] font-medium'>{languageData?.leaderboardsPoints}</p>
-                                </div>
-                                <p className={`saira text-[16px] font-bold ${design === '0' ? 'text-[#FFED63]' : 'gradient-timeCounter'}`}>#{index + 4}</p>
+                                
                             </div>
                         ))
                 }
