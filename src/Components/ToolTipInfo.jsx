9a876348a3d99551110751968075093292384054
Danichok <danielreabyviktorovych@gmail.com>
2024-03-01 02:48:52 +0200

    update

--- src/Components/ToolTipInfo.jsx
+++ src/Components/ToolTipInfo.jsx
@@ -10,30 +10,59 @@ import point3Image from '../images/NewDesign/toolnfo/point3Image.png'
 import what1 from '../images/NewDesign/toolnfo/what1.png'
 import what2 from '../images/NewDesign/toolnfo/what2.png'
 import what3 from '../images/NewDesign/toolnfo/what3.png'
+import money from '../images/NewDesign/header/dollar.png'
+import refferals from '../images/NewDesign/header/points.png'
+import joker from '../images/NewDesign/header/ticket.png'
 
 function ToolTipInfo({ languageData, setToolInfo }) {
     const { design } = useDesign()
 
     return (
         <div className=' w-[97.5%] max-w-[600px] fixed left-[5px] sm:left-[37px] bottom-[30px] sm:bottom-[70px] z-[999999999999] '>
-            <div onClick={e => setToolInfo(false)} className=' cursor-pointer bg-[#0A1225] bg-opacity-90 backdrop-blur-sm mt-[20px] rounded-[12px] lg:rounded-[10x] p-2 lg:p-4 lg:pb-6 '>
+            <div onClick={e => setToolInfo(false)} className=' cursor-pointer bg-[#FFFFFF] mt-[20px] rounded-[12px] lg:rounded-[10x] p-2 lg:p-4 lg:pb-6 '>
                 <div className='flex justify-end '>
-                    <img onClick={e => setToolInfo(false)} className='w-[24px] h-[24px] cursor-pointer' src={design === '0' ? close : require('../images/NewDesign/closeBtn.png')} alt="close" />
+                    <svg onClick={e => setToolInfo(false)} className='w-[24px] h-[24px] cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M20 20L4 4M20 4L4 20" stroke="#1E1E1E" stroke-width="2" stroke-linecap="round" />
+                    </svg>
                 </div>
-                <p className='text-[16px] md:text-[24px] font-semibold text-center text-black gradient-linkDouble max-w-[280px] m-auto'>{languageData.toolInfoTitle}</p>
+                <p className='text-[16px] md:text-[24px] font-semibold text-center text-black max-w-[280px] lg:max-w-[unset] m-auto'>{languageData.toolInfoTitle}</p>
                 <div>
                     <div className='flex justify-around mt-2'>
-                        <p className='text-[14px] text-center font-semibold'>{languageData.toolInfoWhat}</p>
-                        <p className='text-[14px] text-center font-semibold'>{languageData.toolInfoHow}</p>
+                        <div className=' w-full p-1 rounded-l-[24px] flex items-center'>
+                            <div className='w-[40px]'></div>
+                            <p className='lg:w-[unset] text-[14px] text-[#1E1E1E] font-semibold leading-[16px] ml-1'>{languageData.toolInfoLeft1}</p>
+                        </div>
+                        <div className=' w-full p-1 flex items-center '>
+                            <div className='flex justify-between w-full '>
+                                <div className='w-[33%]'>
+                                    <img className='w-[24px] m-auto' src={money} alt="money" />
+                                    <p className='text-[12px] text-[#1E1E1E] font-semibold leading-[16px] text-center mt-1'>{languageData.toolInfoPoint1}</p>
+                                </div>
+                                <div className='w-[33%]'>
+                                    <img className='w-[24px] m-auto' src={refferals} alt="refferals" />
+                                    <p className='text-[12px] text-[#1E1E1E] font-semibold leading-[16px]  text-center mt-2'>{languageData.toolInfoPoint2}</p>
+                                </div>
+                                <div className='w-[33%]'>
+                                    <img className='w-[24px] m-auto' src={joker} alt="joker" />
+                                    <p className='text-[12px] text-[#1E1E1E] font-semibold leading-[16px] text-center mt-2'>{languageData.toolInfoPoint3}</p>
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
-                    <div className='flex justify-around mt-2'>
+                    <div className='flex justify-around'>
                         <div className='toolInfoBg1 w-full p-1 rounded-l-[24px] flex items-center'>
                             <img className='w-[40px]' src={what1} alt="what1" />
                             <p className='w-[115px] lg:w-[unset] saira text-[14px] text-[#1E1E1E] font-semibold leading-[16px] ml-1'>{languageData.toolInfolef1}</p>
                         </div>
-                        <div className='bg-[#FFF2DF] w-full p-1 flex items-center justify-center'>
-                            
-                            <p className=' saira text-[12px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center'>{languageData.toolInfoRight1}</p>
+                        <div className='bg-[#FFF2DF] w-full p-1 flex items-center '>
+                            <div className='flex justify-between w-full '>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>$100</p>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>10</p>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>1</p>
+                            </div>
+
                         </div>
                     </div>
                     <div className='flex justify-around mt-2'>
@@ -42,8 +71,12 @@ function ToolTipInfo({ languageData, setToolInfo }) {
                             <p className='w-[115px] lg:w-[unset] saira text-[14px] text-[#1E1E1E] font-semibold leading-[16px] ml-1'>{languageData.toolInfolef2}</p>
                         </div>
                         <div className='bg-[#EDF1FF] w-full p-1 flex items-center justify-center'>
-                            
-                            <p className=' saira text-[11px] text-[#1E1E1E] font-semibold leading-[14px] ml-1 text-center'>{languageData.toolInfoRight2}</p>
+
+                            <div className='flex justify-between w-full '>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>$100</p>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>10</p>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>1</p>
+                            </div>
                         </div>
                     </div>
                     <div className='flex justify-around mt-2'>
@@ -52,8 +85,11 @@ function ToolTipInfo({ languageData, setToolInfo }) {
                             <p className='w-[115px] lg:w-[unset] saira text-[14px] text-[#1E1E1E] font-semibold leading-[16px] ml-1'>{languageData.toolInfolef3}</p>
                         </div>
                         <div className='bg-[#FFF2DF] w-full p-1 flex items-center justify-center'>
-                            
-                            <p className=' saira text-[11px] text-[#1E1E1E] font-semibold leading-[14px] ml-1 text-center'>{languageData.toolInfoRight3}</p>
+                            <div className='flex justify-between w-full '>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>$100</p>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>10</p>
+                                <p className=' saira text-[16px] text-[#1E1E1E] font-semibold leading-[16px] ml-1 text-center w-[33%]'>1</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -83,16 +119,17 @@ function ToolTipInfo({ languageData, setToolInfo }) {
                 </div>
                 */}
             </div>
-            <svg className='ml-2 mt-[-1px]' width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g filter="url(#filter0_b_1849_11928)">
-                    <path d="M11 14L0.607698 0.499998L21.3923 0.5L11 14Z" fill="#0A1225" fill-opacity="0.7" />
+
+            <svg className='ml-2 mt-[-1px]' xmlns="http://www.w3.org/2000/svg" width="22" height="14" viewBox="0 0 22 14" fill="none">
+                <g filter="url(#filter0_b_63_7684)">
+                    <path d="M11 14L0.607698 0.499998L21.3923 0.5L11 14Z" fill="white" />
                 </g>
                 <defs>
-                    <filter id="filter0_b_1849_11928" x="-13.3923" y="-13.5" width="48.7847" height="41.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <filter id="filter0_b_63_7684" x="-13.3926" y="-13.5" width="48.7852" height="41.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                         <feFlood flood-opacity="0" result="BackgroundImageFix" />
                         <feGaussianBlur in="BackgroundImageFix" stdDeviation="7" />
-                        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_1849_11928" />
-                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_1849_11928" result="shape" />
+                        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_63_7684" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_63_7684" result="shape" />
                     </filter>
                 </defs>
             </svg>
