26d10fa10d703a2761d7366a6506b3522c48a16f
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2024-03-03 18:25:43 +0200

    update

--- src/Components/LeaderboardModal.jsx
+++ src/Components/LeaderboardModal.jsx
@@ -39,7 +39,7 @@ function LeaderboardModal({ setOpen, leaderboardData, loading, languageData, use
                 {
                   leaderboardData?.userRankData &&  <div className='bg-[#EAEAEA] bg-opacity-20 backdrop-blur-md rounded-[12px] p-1 px-1  flex justify-between max-w-[380px] md:max-w-[100%] m-auto'>
                         <div className=' leading-4 text-left m-auto'>
-                            <p className='gradient-linkDouble saira text-[14px] lg:text-[18px] font-bold'>{languageData?.leaderSheetYou} <span className='saira text-[14px] lg:text-[18px] font-bold'> #{leaderboardData?.userRankData[0]?.user_rank || 0},</span> {languageData?.leaderSheetPoints} <span className='saira text-[14px] lg:text-[18px] font-bold'>{Number(user?.points).toFixed(0)}</span></p>
+                            <p className='gradient-linkDouble saira text-[14px] lg:text-[18px] font-bold'>{languageData?.leaderSheetYou} <span className='saira text-[14px] lg:text-[18px] font-bold'> #{leaderboardData?.userRankData[0]?.position || 0},</span> {languageData?.leaderSheetPoints} <span className='saira text-[14px] lg:text-[18px] font-bold'>{Number(user?.points).toFixed(0)}</span></p>
                         </div>
                     </div>
                 }
