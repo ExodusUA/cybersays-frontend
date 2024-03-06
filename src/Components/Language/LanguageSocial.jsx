cb0e62fc574ebf19122c2032fd7d72d76fd29618
Danichok <danielreabyviktorovych@gmail.com>
2024-03-06 03:20:35 +0200

    update

---
+++ src/Components/Language/LanguageSocial.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import LanguageSwitcher from './LangSwitch';
+import { useLanguage } from '../../Helpers/Languages/LanguageContext';
+import { useDesign } from '../../Helpers/Design/DesignContext'
+
+function LanguageSocial() {
+    const [langOpen, setLangOpen] = React.useState(false);
+    const { language } = useLanguage();
+
+    const { design } = useDesign()
+
+    function getLanguageName() {
+        switch (language) {
+            case 'en':
+                return 'EN';
+            case 'es':
+                return 'ES';
+            case 'pt':
+                return 'PT';
+            default:
+                return 'EN';
+        }
+    }
+
+    return (
+        <div className=' relative'>
+            <div>
+                <button onClick={() => {
+
+                    setLangOpen(!langOpen)
+                }} className={`bg-[#626161]  border-[2px]  ${design === '0' ? 'border-[#FFED63] rounded-[50px]' : 'border-[#A2DBF0] rounded-[14px]'}  flex items-center gap-2 px-[20px] py-[10px]`}>
+                    <p className='text-white font-bold text-[16px]'>{getLanguageName()}</p>
+                    <img className='w-[20px]' src="https://img.icons8.com/ios-filled/50/ffffff/expand-arrow--v1.png" alt="Arrow" />
+                </button>
+            </div>
+
+            {langOpen && (
+                <div className={`mt-0 absolute top-[60px] p-4 border-[2px] ${design === '0' ? 'border-[#FFED63] ' : 'border-[#A2DBF0]'} bg-[#626161]  w-[100px] rounded-[12px] py-4 min-w-[50px]  left-0`}>
+                    <LanguageSwitcher setIsLangOpen={setLangOpen} />
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default LanguageSocial;
--- src/Pages/Terms.jsx
+++ src/Pages/Terms.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import HeaderProfile from '../Components/HeaderProfile'
-import termsData from '../Pages/terms_paragraphs.json';
+import termsData from '../Pages/socialPageJSON/terms_en.json';
+import LanguageSocial from '../Components/Language/LanguageSocial';
 
 function Terms() {
 
     return (
         <div>
             <HeaderProfile />
+            <div className='absolute right-2 lg:right-[20px] top-1 lg:top-3 flex items-center align-start z-20'>
+                <LanguageSocial />
+            </div>
+
             <div className='max-w-[1300px] px-2 m-auto overflow-scroll h-screen pb-[50px] mt-[20px]'>
                 {termsData.map((paragraph, index) => (
                     <p className='text-black saira text-[14px] lg:text-[16px] mt-2' key={index}>{paragraph}</p>
