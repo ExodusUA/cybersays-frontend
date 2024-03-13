c902482280ecadbf0bc17520c3909279c981c57c
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2024-03-13 17:06:21 +0200

    upd

--- src/Pages/CyberSaysPages/Refferals.jsx
+++ src/Pages/CyberSaysPages/Refferals.jsx
@@ -61,8 +61,8 @@ function Refferals({ user, languageData, setReferralsOpen, dataMessage, setOpenM
             navigator
                 .share({
                     title: document.title,
-                    text: dataMessage[selectedMessage]?.desc + '. Image Link: ' + 'https://' + window.location.host + uploadedPhotos[selectedGif],
-                    url: '/' + user?.referral_code,
+                    text: dataMessage[selectedMessage]?.desc + '. Image Link: ' + 'http://' + window.location.host + uploadedPhotos[selectedGif],
+                //url: '/' + user?.referral_code,
                     
                 })
                 .then(() => console.log('Successful share! 🎉'))
