add6f6a2bdd4bae5b338e3742f9c8c2985b29657
Danichok <danielreabyviktorovych@gmail.com>
2024-03-13 20:50:33 +0200

    update login

--- src/Components/CircleNavigation.jsx
+++ src/Components/CircleNavigation.jsx
@@ -126,7 +126,7 @@ function CircleNavigation({ setActivePageIndex, activePageIndex, languageData, u
                         <div className={`text-black relative text-center duration-200 flex justify-center  m-auto  ${currentSlide === 1 ? 'top-0' : 'top-8 md:top-[60px]'}`}>
                             <div>
                                 <img className={`w-[32px] h-[32px] md:w-[48px] md:h-[48px] m-auto ${design === '0' ? '' : currentSlide === 1 ? 'border-[2px] border-[#FDA52F] rounded-full' : 'border-[2px] border-[#A2DBF0] rounded-full'}`} src={design === '0' ? link3 : require('../images/NewDesign/circleMenu/page3.png')} alt="link3" />
-                                <p className='saira text-[12px] md:text-[16px] font-semibold md:mt-2 '>{languageData?.NamePage2}</p>
+                                <p className='saira text-[12px] md:text-[16px] w-[200px] font-semibold md:mt-2 '>{languageData?.NamePage2}</p>
                             </div>
                         </div>
                     </SwiperSlide>
@@ -134,7 +134,7 @@ function CircleNavigation({ setActivePageIndex, activePageIndex, languageData, u
                         <div className={`text-black relative text-center duration-200 flex justify-center  ${currentSlide === 2 ? 'top-0' : 'top-8 md:top-[60px]'}`}>
                             <div>
                                 <img className={`w-[32px] h-[32px] md:w-[48px] ${design === '0' ? 'md:h-[42px]' : 'md:h-[48px]'}  m-auto ${design === '0' ? '' : currentSlide === 2 ? 'border-[2px] border-[#FDA52F] rounded-full' : 'border-[2px] border-[#A2DBF0] rounded-full'}`} src={design === '0' ? link5 : require('../images/NewDesign/circleMenu/page2.png')} alt="link5" />
-                                <p className='saira text-[12px] md:text-[16px] font-semibold md:mt-2'>{languageData?.NamePage3}</p>
+                                <p className='saira text-[12px] md:text-[16px] w-[200px] font-semibold md:mt-2'>{languageData?.NamePage3}</p>
                             </div>
                         </div>
                     </SwiperSlide>
--- src/Components/Language/Language.jsx
+++ src/Components/Language/Language.jsx
@@ -1,35 +1,49 @@
 import React from 'react';
 import LanguageSwitcher from './LangSwitch';
 import { useLanguage } from '../../Helpers/Languages/LanguageContext';
+import flag_usa from '../../images/flags/flag-en.png'
+import flag_es from '../../images/flags/flag_es.png'
+import flag_pt from '../../images/flags/flag-pt.png'
 
 function Language() {
     const [langOpen, setLangOpen] = React.useState(false);
     const { language } = useLanguage();
 
-    function getLanguageName() {
+    function getLanguageInfo() {
         switch (language) {
             case 'en':
-                return 'EN';
+                return { name: 'EN', flag: flag_usa };
             case 'es':
-                return 'ES';
+                return { name: 'ES', flag: flag_es };
             case 'pt':
-                return 'PT';
+                return { name: 'PT', flag: flag_pt };
             default:
-                return 'EN';
+                return { name: 'EN', flag: flag_usa };
         }
     }
-
+    const { name, flag } = getLanguageInfo();
     return (
         <div>
             <div>
-                <button onClick={() => setLangOpen(!langOpen)} className='gradient-tourToggle rounded-[8px] lg:rounded-[12px] flex items-center gap-2 px-[12px] lg:px-[25px] py-[7px] lg:py-[10px] mr-4'>
-                    <p className='text-white font-bold text-[14px] md:text-[18px]'>{getLanguageName()}</p>
-                    <img className='w-[20px]' src="https://img.icons8.com/ios-filled/50/ffffff/expand-arrow--v1.png" alt="Arrow" />
+                <button onClick={() => setLangOpen(!langOpen)} className='gradient-tourToggle langaugeBtnShadow outline-none rounded-[8px] lg:rounded-[12px] flex items-center gap-2 px-[12px] lg:px-[25px] py-[7px] lg:py-[10px] mr-4'>
+
+                    <p className='text-white font-bold text-[14px] md:text-[16px]'>{name}</p>
+                    <img src={flag} alt={name} className="w-[26px] h-[18px]" />
+                    {
+                        langOpen
+                            ?
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2 17L12 7L22 17" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            : <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M22 7L12 17L2 7" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                    }
                 </button>
             </div>
 
             {langOpen && (
-                <div className='mt-0 absolute top-[40px] md:top-[55px]  bg-[#83869b] bg-opacity-20 backdrop-blur-lg rounded-[8px] lg:rounded-[12px] pb-2 lg:pb-0 pt-2 lg:pt-2 w-[75px] md:w-[107px] right-4 lg:right-0 md:left-0'>
+                <div className='mt-0 absolute top-[43px] md:top-[55px] border-[1px] border-[#DDBBFD] bg-[#83869b] bg-opacity-20 backdrop-blur-lg rounded-[8px] lg:rounded-[12px] pb-2 lg:pb-0 pt-2 lg:pt-2 w-[112px] md:w-[141px] right-4 lg:right-0 md:left-0'>
                     <LanguageSwitcher setIsLangOpen={setLangOpen} />
                 </div>
             )}
