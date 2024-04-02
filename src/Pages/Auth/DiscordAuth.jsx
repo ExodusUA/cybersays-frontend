7f000cf84dd037091a6754acc187aa951a27783e
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2024-04-03 01:21:42 +0300

    Update DiscordAuth.jsx

--- src/Pages/Auth/DiscordAuth.jsx
+++ src/Pages/Auth/DiscordAuth.jsx
@@ -1,46 +1,45 @@
-import React, { useEffect } from 'react'
-import { discordUserAuth } from '../../Requests/auth';
-import { useNavigate } from 'react-router-dom';
-import { getUserCountry } from '../../Requests/utills';
+import React, { useEffect } from "react";
+import { discordUserAuth } from "../../Requests/auth";
+import { useNavigate } from "react-router-dom";
+import { getUserCountry } from "../../Requests/utills";
 
 function DiscordAuth() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const queryParams = new URLSearchParams(window.location.search);
+  const fragment = new URLSearchParams(window.location.hash.slice(1));
+  const [accessToken, tokenType] = [fragment.get("access_token"), fragment.get("token_type")];
 
-    const queryParams = new URLSearchParams(window.location.search);
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
+  let refferalCode = queryParams.get("ref") || window.localStorage.getItem("ref");
+  if (refferalCode === undefined || refferalCode === null) refferalCode = null;
 
-    let refferalCode = queryParams.get('ref') || window.localStorage.getItem('ref');
-    if (refferalCode === undefined || refferalCode === null) refferalCode = null
-
-    useEffect(() => {
-
-        const auth = async () => {
-            try {
-                let userCountry = await getUserData();
-                const res = await discordUserAuth(accessToken, tokenType, userCountry.country, refferalCode)
+  useEffect(() => {
+    const auth = async () => {
+      try {
+        let userCountry = await getUserData();
+        const res = await discordUserAuth(accessToken, tokenType, userCountry.country, refferalCode);
+        window.location.replace("https://cybersaysm-redirect.vercel.app?token=" + res.data.token);
+        /*
                 localStorage.setItem('token', res.data.token);
                 navigate('/')
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        auth()
-    }, [])
+                */
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    auth();
+  }, []);
 
-    const getUserData = async () => {
-        try {
-            const res = await getUserCountry();
-            return res.data.Data
-        } catch (error) {
-            console.log(error)
-        }
+  const getUserData = async () => {
+    try {
+      const res = await getUserCountry();
+      return res.data.Data;
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div className='text-black saira'>Loading...</div>
-    )
+  return <div className="saira text-black">Loading...</div>;
 }
 
-export default DiscordAuth
+export default DiscordAuth;
